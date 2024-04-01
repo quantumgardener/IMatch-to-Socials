3c15de5f71287b3f917e8dc178bf89ff2a290e58
David C. Buchan <david@buchan.org>
2024-04-01 19:49:09 +1100

    Add groups and keywords. Save image into cache

--- IMatch to flickr.py
+++ IMatch to flickr.py
@@ -39,28 +39,58 @@ flickr = flickr_support.getFlickrInfo(api, refresh=False)  # True to force reloa
 
 count = 1
 for id in im['missing']:
-    logging.info(f"Uploading: {im['data'][id]['filename']} ({im['data'][id]['size']/MB:2.1f} MB) ({count}/{len(im['missing'])})")
+    image = im['data'][id]
+    logging.info(f"Uploading: {image['filename']} ({image['size']/MB:2.1f} MB) ({count}/{len(im['missing'])})")
+
+    pp.pprint(image)
   
     resp = api.upload(
-        im['data'][id]['filename'],
-        title = im['data'][id]['title'] if im['data'][id]['title'] != '' else im['data'][id]['filename'],
-        description = im['data'][id]['description'],
-        is_public=0, # 0 for hidden and testing, 1 for public
+        image['filename'],
+        title = image['title'] if image['title'] != '' else image['filename'],
+        description = image['description'],
+        is_public=1, # 0 for hidden and testing, 1 for public
         is_friend=0,
         is_family=0
     )
     from xml.etree import ElementTree as ET
     photo_id = resp.findtext('photoid')
     
-    for album in im['data'][id]['albums']:
+    for album in image['albums']:
         resp = api.photosets_addPhoto(photoset_id=album, photo_id=photo_id)
 
+    for group in image['groups']:
+        resp = api.groups_pools_add(group_id=group, photo_id=photo_id)
+
+    # flickr will bring in hierarchical keywords not under our control as level|level|level
+    # which frankly is stupid. Easiest way is to delete them all. We don't know quite what
+    # it will have loaded.
+    resp = api.photos.getInfo(photo_id = photo_id, format = "parsed-json")
+    for badtag in resp['photo']['tags']['tag']:
+        resp = api.photos.removeTag(tag=badtag['id'], format="parsed-json")
+    
+    # Now add back the "Approved" tags
+    resp = api.photos.addTags(tags=",".join(image['keywords']), photo_id=photo_id)
+
+    # ---------------------------------------------------------------------------------
+    # Start the clean up now
+        
     # Update the image in IMatch by adding the attributes below.
+    posted = datetime.now().isoformat()[:10]
     im['api'].setAttributes("flickr", [id], data = {
-        'posted' : datetime.now().isoformat()[:10],
+        'posted' : posted,
         'photo_id' : photo_id,
         'url' : f"https://www.flickr.com/photos/dcbuchan/{photo_id}"
     })
+
+    # Add image into the flickr construct for caching next time.
+    flickr["{photo_id}"] = {
+        "title"  : im['data'][id]['title'],
+        "albums" : im['data'][id]['albums'],
+        "groups" : im['data'][id]['groups'],
+        "posted" : posted
+    }
+    flickr_support.saveFlickrInfo(flickr)
+
     count += 1
     
 
